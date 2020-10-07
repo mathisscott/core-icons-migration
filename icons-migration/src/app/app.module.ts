ccc9d40492d3501872bdd027ffae20a99a16fbdb
Scott Mathis <smathis@vmware.com>
2020-10-07 14:48:30 -0500

    aliasing icons and service

--- icons-migration/src/app/app.module.ts
+++ icons-migration/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerElementSafely } from '@clr/core/internal';
 
 // import the Clarity Core ClarityIcons service. you will use this to add icon shapes to the library as you need them.
 // import icon shapes from the library
 import {
-  ClarityIcons,
+  CdsIcon,
+  ClarityIcons as IconService,
   homeIcon,
   successStandardIcon,
   infoStandardIcon,
@@ -22,13 +24,12 @@ const [, imageIconShape] = imageIcon;
 // use the addIcons() method to add the shapes
 // alternately, you can destructure an icon shape and assign any name you want to it (as demonstrated with the image icon)
 // This gives teams a great deal of freedom in storing and retrieving icons in their application.
-ClarityIcons.addIcons(homeIcon, successStandardIcon, infoStandardIcon, [
-  'image',
+IconService.addIcons(homeIcon, successStandardIcon, infoStandardIcon, [
+  'my-image',
   imageIconShape,
 ]);
 
-// Import the register file for the icon web component. This file adds the cds-icon component to the browser.
-import '@clr/core/icon/register';
+registerElementSafely('my-icon', CdsIcon);
 
 // The Angular application needs the CUSTOM_ELEMENTS_SCHEMA defined on the application module.
 @NgModule({
